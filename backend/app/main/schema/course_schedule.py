d3ffca3780ad984f4fd508a3b6721506fdd84b85
shamming <1685087768@qq.com>
2025-03-30 15:23:18 +0800

    refactor: let GetCourseScheduleDetail inherit from SchemaBase

--- backend/app/main/schema/course_schedule.py
+++ backend/app/main/schema/course_schedule.py
@@ -5,14 +5,20 @@ from datetime import date
 
 class CourseScheduleSchemaBase(SchemaBase):
     id: int
-    # course_id: int
+    course_id: int
     schedule_date: date
     day_of_week: str
     start_time: str
     end_time: str
     location: str
 
-class GetCourseScheduleDetail(CourseScheduleSchemaBase):
-    # 这个很重要 返回给前端的数据必须是padantic的模型 这里允许从sqlalchemy的模型中直接转换
+class GetCourseScheduleDetail(SchemaBase):
     model_config = ConfigDict(from_attributes=True)
-    pass
+    
+    id: int
+    # course_id: int  # 不需要返回课程ID
+    schedule_date: date
+    day_of_week: str
+    start_time: str
+    end_time: str
+    location: str
